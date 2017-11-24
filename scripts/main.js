49d09e37249e950482f97ba924f74bec87e8e142
xaxnas <32159369+xaxnas@users.noreply.github.com>
2017-11-24 17:50:05 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ var myImage = document.getElementById('img_peng1');
 var ClickButton = document.getElementById("ClickButton");
 var ClickedCountText = document.getElementById("ClickedCountHTML");
 
-var ClickCount = parseFloat(get_cookie("ClickCount"));
+var ClickCount = parseFloat(get_cookie("ClickCount"), 0);
 
 function Migalka() {
 	var mySrc = myImage.getAttribute('src');
@@ -29,12 +29,8 @@ ClickButton.onclick = function ClickButtonOnClick() {
 }
 
 var stopButton = document.getElementById("stop");
-var stopButtonTriggered;
-if(get_cookie("stopButtonTriggered")==="false") {
-	stopButtonTriggered = false;
-	} else {
-		stopButtonTriggered = true;
-	}
+var stopButtonTriggered = (get_cookie("stopButtonTriggered", "false")==="true");
+
 t=setInterval(Migalka,100);
 
 
@@ -52,13 +48,13 @@ function stopButtonOnClick() {
 stopButton.onclick = stopButtonOnClick;
 //---------------------------------------------autoclick-----------------------------
 var AutoClickButton = document.getElementById("AutoClickButton");
-if(parseFloat(get_cookie("AutoClickCost"))===0) AutoClickCost = 50; else AutoClickCost = parseFloat(get_cookie("AutoClickCost"));
-var AutoClickAmount = parseInt(get_cookie("AutoClickAmount"));
+var AutoClickCost = parseFloat(get_cookie("AutoClickCost", 50));
+var AutoClickAmount = parseInt(get_cookie("AutoClickAmount", 0));
 var AutoClickPower = 0.1;
 AutoClickButton.onclick = function AutoClickButtonOnClick() {
 	if(ClickCount>=AutoClickCost) {
-		ClickCount = Math.round((ClickCount - AutoClickCost)*10)/10;
-		AutoClickCost = Math.round(AutoClickCost*1.25*10)/10;
+		ClickCount = RoundTo(ClickCount - AutoClickCost);
+		AutoClickCost = RoundTo(AutoClickCost*1.25);
 		AutoClickAmount = AutoClickAmount + 1;
 		AutoClickButton.childNodes[0].nodeValue = "auto[" + AutoClickAmount + "] ["+AutoClickCost+"click]";
 		ClickedCountText.childNodes[0].nodeValue = "clicked: " + ClickCount;
@@ -66,13 +62,13 @@ AutoClickButton.onclick = function AutoClickButtonOnClick() {
 }
 //---------------------------------------------every second--------------------------
 timeSecond=setInterval(EverySecondFunction, 1000);
-var second=parseInt(get_cookie("second")); var secondString="00";
-var minute=parseInt(get_cookie("minute")); var minuteString="00:";
-var hour=parseInt(get_cookie("hour")); var hourString="00:";
+var second=parseInt(get_cookie("second", 0)); var secondString="00";
+var minute=parseInt(get_cookie("minute", 0)); var minuteString="00:";
+var hour=parseInt(get_cookie("hour", 0)); var hourString="00:";
 
 function EverySecondFunction() {
-	second=second+1;
-	ClickCount = Math.round((ClickCount + (Math.round(AutoClickAmount*AutoClickPower*10)/10))*10)/10;
+	second=second+1; 
+	ClickCount = RoundTo(ClickCount + (AutoClickAmount*AutoClickPower));
 	if (second===60) {minute=minute+1; second=0;}
 	if (minute===60) {hour=hour+1; minute=0;}
 	if (second<10) secondString="0"+second; else secondString=second;
@@ -81,20 +77,24 @@ function EverySecondFunction() {
 	ClickedCountText.childNodes[0].nodeValue = "clicked: " + ClickCount;
 	document.getElementById("TimeElapsed").childNodes[0].nodeValue = "time elapsed: "+hourString+minuteString+secondString;
 }
+//---------------------------------------------round---------------------------------
+function RoundTo(value) {
+	return Math.round(value*10)/10;
+}
 //---------------------------------------------cookies-------------------------------
-function set_cookie (name, value) {
-   document.cookie = name + "=" + encodeURI(value.toString()) + "; expires = Thu, 18 Dec 2019 12:00:00 UTC; path=/";
+function set_cookie (name) {
+   document.cookie = name + "=" + encodeURI(name.toString()) + "; expires = Thu, 18 Dec 2019 12:00:00 UTC; path=/";
 } //save cookie
 function SaveCookie() {
-	set_cookie("ClickCount", ClickCount);
-	set_cookie("stopButtonTriggered", stopButtonTriggered);
-	set_cookie("second", second); set_cookie("minute", minute);  set_cookie("hour", hour);
-	set_cookie("AutoClickCost", AutoClickCost); set_cookie("AutoClickAmount", AutoClickAmount); set_cookie("AutoClickPower", AutoClickPower);
+	set_cookie("ClickCount");
+	set_cookie("stopButtonTriggered");
+	set_cookie("second"); set_cookie("minute");  set_cookie("hour");
+	set_cookie("AutoClickCost"); set_cookie("AutoClickAmount"); set_cookie("AutoClickPower");
 } //add here what to save
 var SaveButton = document.getElementById("SaveButton");
 SaveButton.onclick = SaveCookie; //call by click
 
-function get_cookie (cookie_name) {
+function get_cookie (cookie_name, defaut) {
     var nameEQ = cookie_name + "=";
     var ca = document.cookie.split(';');
     for(var i=0;i < ca.length;i++) {
@@ -102,7 +102,7 @@ function get_cookie (cookie_name) {
         while (c.charAt(0)==' ') c = c.substring(1,c.length);
         if (c.indexOf(nameEQ) == 0) return decodeURI(c.substring(nameEQ.length,c.length));
     }
-    return 0;
+    return defaut;
 } //read cookies
 //-----------------------------------------start page values----------------------------------
 window.onload = function load_start_cookie_from_page() {
